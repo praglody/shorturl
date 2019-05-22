1874629e18fe63683b537be6071307ebc995e4d2
praglody <praglody@163.com>
2019-05-22 10:02:47 +0800

    mysql

--- app/app.go
+++ app/app.go
@@ -3,9 +3,13 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"shorturl/lib/dao"
 )
 
 func Run() {
+	res := dao.GetRow("qwerty")
+	fmt.Println("查询结果", res)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello, short url")
 	})
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shorturl
 
 go 1.12
+
+require github.com/go-sql-driver/mysql v1.4.1
